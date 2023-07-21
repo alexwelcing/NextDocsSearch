c6a40e4a3a5882e0378da5bd624eafa070d4941b
alexwelcing <alexwelcing@gmail.com>
2023-07-20 23:46:22 -0400

    test tree

--- components/SearchDialog.tsx
+++ components/SearchDialog.tsx
@@ -72,16 +72,19 @@ export function SearchDialog() {
   }
 
   function setQuestionsBasedOnSelection(selectedKey: string) {
+    const questionText = currentQuestions.find(q => q.key === selectedKey)?.text || '';
+    setQuery(questionText);  // This will set the input field value even for root questions.
+
     const nextQuestionsObj = QUESTIONS_TREE[selectedKey as QuestionKey];
     if (nextQuestionsObj) {
-      const nextQuestionsArray: [string, string][] = Object.entries(nextQuestionsObj);
-      setCurrentQuestions(nextQuestionsArray.map(([key, text]) => ({ key, text })));
+        const nextQuestionsArray: [string, string][] = Object.entries(nextQuestionsObj);
+        setCurrentQuestions(nextQuestionsArray.map(([key, text]) => ({ key, text })));
     } else {
-      const questionText = currentQuestions.find(q => q.key === selectedKey)?.text || '';
-      setQuery(questionText);
-      complete(questionText);
+        complete(questionText);
     }
 }
+
+
 
 const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
   e.preventDefault();
@@ -127,7 +130,42 @@ return (
           </DialogHeader>
           <form onSubmit={handleSubmit}>
             <div className="grid gap-4 py-4 text-slate-700">
-              {/* Text Edit Box */}
+              {query && (
+                <div className="flex gap-4">
+                  <span className="bg-slate-100 dark:bg-slate-300 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center">
+                    <User width={18} />{' '}
+                  </span>
+                  <p className="mt-0.5 font-semibold text-slate-700 dark:text-slate-100">{query}</p>
+                </div>
+              )}
+
+              {isLoading && (
+                <div className="animate-spin relative flex w-5 h-5 ml-2">
+                  <Loader />
+                </div>
+              )}
+
+              {error && (
+                <div className="flex items-center gap-4">
+                  <span className="bg-red-100 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center">
+                    <Frown width={18} />
+                  </span>
+                  <span className="text-slate-700 dark:text-slate-100">
+                    Sad news, the search has failed! Please try again.
+                  </span>
+                </div>
+              )}
+
+              {completion && !error ? (
+                <div className="flex items-center gap-4 dark:text-white">
+                  <span className="bg-green-500 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center">
+                    <Wand width={18} className="text-white" />
+                  </span>
+                  <h3 className="font-semibold">Answer:</h3>
+                  {completion}
+                </div>
+              ) : null}
+
               <div className="relative">
                 <Input
                   placeholder="Ask a question..."
@@ -142,8 +180,6 @@ return (
                   }`}
                 />
               </div>
-
-              {/* ... (rest of your form content) */}
               <div className="text-xs text-gray-500 dark:text-gray-100">
                 Or try:
                 {currentQuestions.map(({ key, text }) => (
