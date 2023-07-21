222cd573a5e3ad85b23a8ed2ced8922e1d2b2eaf
alexwelcing <alexwelcing@gmail.com>
2023-07-20 23:17:26 -0400

    decision tree

--- components/SearchDialog.tsx
+++ components/SearchDialog.tsx
@@ -14,52 +14,91 @@ import { Input } from '@/components/ui/input'
 import { useCompletion } from 'ai/react'
 import { X, Loader, User, Frown, CornerDownLeft, Search, Wand } from 'lucide-react'
 
-export function SearchDialog() {
-  const [open, setOpen] = React.useState(false)
-  const [query, setQuery] = React.useState<string>('')
+type QuestionKey = 'root' | 'A' | 'B';
+type Question = {
+  key: string;
+  text: string;
+};
 
+type QuestionTreeNode = Record<string, string>;
+
+const QUESTIONS_TREE: Record<QuestionKey, QuestionTreeNode> = {
+  root: {
+    A: "Who is Alex?",
+    B: "Where is Alex?"
+  },
+  A: {
+    C: "Has he worked anywhere?",
+    D: "Does he have skills?",
+    E: "What has he accomplished?"
+  },
+  B: {
+    F: "Is it nice there?",
+    G: "Can he travel?",
+    H: "Will he come into an office?"
+  }
+};
+
+export function SearchDialog() {
+  const [open, setOpen] = React.useState(false);
+  const [query, setQuery] = React.useState<string>('');
   const { complete, completion, isLoading, error } = useCompletion({
     api: '/api/vector-search',
-  })
+  });
+
+  const [currentQuestions, setCurrentQuestions] = React.useState<Question[]>(
+    Object.entries(QUESTIONS_TREE.root).map(([key, text]) => ({ key, text }))
+  );
 
   React.useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === 'k' && e.metaKey) {
-        setOpen(true)
+        setOpen(true);
       }
 
       if (e.key === 'Escape') {
-        console.log('esc')
-        handleModalToggle()
+        handleModalToggle();
       }
-    }
+    };
 
-    document.addEventListener('keydown', down)
-    return () => document.removeEventListener('keydown', down)
-  }, [])
+    document.addEventListener('keydown', down);
+    return () => document.removeEventListener('keydown', down);
+  }, [handleModalToggle]);
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   function handleModalToggle() {
-    setOpen(!open)
-    setQuery('')
+    setOpen(!open);
+    setQuery('');
   }
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault()
-    console.log(query)
-    complete(query)
-  }
+  function setQuestionsBasedOnSelection(selectedKey: string) {
+    const nextQuestionsObj = QUESTIONS_TREE[selectedKey as QuestionKey];
+    if (nextQuestionsObj) {
+      const nextQuestionsArray: [string, string][] = Object.entries(nextQuestionsObj);
+      setCurrentQuestions(nextQuestionsArray.map(([key, text]) => ({ key, text })));
+    } else {
+      const questionText = currentQuestions.find(q => q.key === selectedKey)?.text || '';
+      setQuery(questionText);
+      complete(questionText);
+    }
+}
 
-  return (
-    <>
-      <button
-        onClick={() => setOpen(true)}
-        className="text-base flex gap-2 items-center px-4 py-2 z-50 relative
-        text-slate-500 dark:text-slate-400  hover:text-slate-700 dark:hover:text-slate-300
-        transition-colors
-        rounded-md
-        border border-slate-200 dark:border-slate-500 hover:border-slate-300 dark:hover:border-slate-500
-        min-w-[300px] "
-      >
+const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+  e.preventDefault();
+  complete(query);
+};
+
+return (
+  <>
+    <button
+      onClick={() => setOpen(true)}
+      className="text-base flex gap-2 items-center px-4 py-2 z-50 relative
+      text-slate-500 dark:text-slate-400  hover:text-slate-700 dark:hover:text-slate-300
+      transition-colors
+      rounded-md
+      border border-slate-200 dark:border-slate-500 hover:border-slate-300 dark:hover:border-slate-500
+      min-w-[300px] "
+    >
         <Search width={15} />
         <span className="border border-l h-5"></span>
         <span className="inline-block ml-4">Search...</span>
@@ -86,45 +125,9 @@ export function SearchDialog() {
               <X className="h-4 w-4 dark:text-gray-100" />
             </button>
           </DialogHeader>
-
           <form onSubmit={handleSubmit}>
             <div className="grid gap-4 py-4 text-slate-700">
-              {query && (
-                <div className="flex gap-4">
-                  <span className="bg-slate-100 dark:bg-slate-300 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center">
-                    <User width={18} />{' '}
-                  </span>
-                  <p className="mt-0.5 font-semibold text-slate-700 dark:text-slate-100">{query}</p>
-                </div>
-              )}
-
-              {isLoading && (
-                <div className="animate-spin relative flex w-5 h-5 ml-2">
-                  <Loader />
-                </div>
-              )}
-
-              {error && (
-                <div className="flex items-center gap-4">
-                  <span className="bg-red-100 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center">
-                    <Frown width={18} />
-                  </span>
-                  <span className="text-slate-700 dark:text-slate-100">
-                    Sad news, the search has failed! Please try again.
-                  </span>
-                </div>
-              )}
-
-              {completion && !error ? (
-                <div className="flex items-center gap-4 dark:text-white">
-                  <span className="bg-green-500 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center">
-                    <Wand width={18} className="text-white" />
-                  </span>
-                  <h3 className="font-semibold">Answer:</h3>
-                  {completion}
-                </div>
-              ) : null}
-
+              {/* Text Edit Box */}
               <div className="relative">
                 <Input
                   placeholder="Ask a question..."
@@ -139,40 +142,34 @@ export function SearchDialog() {
                   }`}
                 />
               </div>
+
+              {/* ... (rest of your form content) */}
               <div className="text-xs text-gray-500 dark:text-gray-100">
-                Or try:{' '}
-                <button
-                  type="button"
-                  className="px-1.5 py-0.5
-                  bg-slate-50 dark:bg-gray-500
-                  hover:bg-slate-100 dark:hover:bg-gray-600
-                  rounded border border-slate-200 dark:border-slate-600
-                  transition-colors"
-                  onClick={(_) => setQuery('What are embeddings?')}
-                >
-                  What are embeddings?
-                </button>
-                <button
-                  type="button"
-                  className="px-1.5 py-0.5
-                  bg-slate-50 dark:bg-gray-500
-                  hover:bg-slate-100 dark:hover:bg-gray-600 space-x-2
-                  rounded border border-slate-200 dark:border-slate-600
-                  transition-colors"
-                  onClick={(_) => setQuery('Who is Alex Welcing?')}
-                >
-                  Who is Alex?
-                </button>
+                Or try:
+                {currentQuestions.map(({ key, text }) => (
+                  <button
+                    key={key}
+                    type="button"
+                    className="px-1.5 py-0.5
+                    bg-slate-50 dark:bg-gray-500
+                    hover:bg-slate-100 dark:hover:bg-gray-600 space-x-2
+                    rounded border border-slate-200 dark:border-slate-600
+                    transition-colors"
+                    onClick={() => setQuestionsBasedOnSelection(key)}
+                  >
+                    {text}
+                  </button>
+                ))}
               </div>
+              <DialogFooter>
+                <Button type="submit" className="bg-red-500">
+                  Ask
+                </Button>
+              </DialogFooter>
             </div>
-            <DialogFooter>
-              <Button type="submit" className="bg-red-500">
-                Ask
-              </Button>
-            </DialogFooter>
           </form>
         </DialogContent>
       </Dialog>
     </>
-  )
+  );
 }
