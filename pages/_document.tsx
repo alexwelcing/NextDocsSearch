cb7ce681be0b4cfc41b966b893f47f3e8f57f0a7
alexwelcing <alexwelcing@gmail.com>
2023-07-27 10:34:49 -0400

    add ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-HZNLDCC1GR" />
       <Head />
       <body>
         <Main />
