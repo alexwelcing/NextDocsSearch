fd9c89b235fc9c733058c0455d38cec3cb164eb7
alexwelcing <alexwelcing@gmail.com>
2023-08-09 17:44:02 -0400

    sorry search

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,18 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
-import { SearchDialog } from '@/components/SearchDialog'
-import Footer from '@/components/ui/footer'
-import ThreeSixty from '@/components/ThreeSixty'
-import CircleNav from '@/components/ui/CircleNav'
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
+import styles from '@/styles/Home.module.css';
+import { SearchDialog } from '@/components/SearchDialog';
+import Footer from '@/components/ui/footer';
+import ThreeSixty from '@/components/ThreeSixty';
+import CircleNav from '@/components/ui/CircleNav';
 
 const Chat = () => {
-
-  const [currentImage, setCurrentImage] = useState<string>('setCurrentImage')
-  const [backgroundImages, setBackgroundImages] = useState<string[]>([])
+  const [currentImage, setCurrentImage] = useState<string>('./background/bg1.jpg'); // Initial placeholder image
+  const [backgroundImages, setBackgroundImages] = useState<string[]>([]);
 
   useEffect(() => {
-    // Use requestIdleCallback to fetch images when the browser is idle
     const fetchImagesWhenIdle = () => {
       fetch('/api/backgroundImages')
         .then((response) => {
@@ -22,54 +20,50 @@ const Chat = () => {
           return response.json();
         })
         .then((images) => {
-          setBackgroundImages(images)
-          // Preload the images
+          setBackgroundImages(images);
           images.forEach((image: any) => {
-            new Image().src = `./background/${image}`
-          })
-          const randomImage = images[Math.floor(Math.random() * images.length)]
-          setCurrentImage(`./background/${randomImage}`)
+            new Image().src = `./background/${image}`;
+          });
+          const randomImage = images[Math.floor(Math.random() * images.length)];
+          setCurrentImage(`./background/${randomImage}`);
         })
         .catch((error) => {
           console.error('There was a problem fetching the background images:', error);
         });
-    }
+    };
 
     if ('requestIdleCallback' in window) {
       (window as any).requestIdleCallback(fetchImagesWhenIdle);
     } else {
-      // If requestIdleCallback is not supported, fallback to a simple setTimeout
       setTimeout(fetchImagesWhenIdle, 1000);
     }
-
-  }, [])
+  }, []);
 
   const handleImageChange = () => {
-    const randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)]
-    setCurrentImage(`./background/${randomImage}`)
-  }
+    const randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
+    setCurrentImage(`./background/${randomImage}`);
+  };
 
   return (
     <>
       <Head>
         <title>Chat with Alex documents while enjoying fantastic generated worlds!</title>
-        <meta name="description" content="Explore Alex Welcing's world using 360 and chat with documents! Learn about his experience in product leadership and how he uses generative AI and LLM to build amazing products." />
+        <meta
+          name="description"
+          content="Explore Alex Welcing's world using 360 and chat with documents! Learn about his experience in product leadership and how he uses generative AI and LLM to build amazing products."
+        />
         <meta name="keywords" content="Alex Welcing, product leadership, generative AI, LLM, 360, chat with documents" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <CircleNav />
       <main className={`${styles.main} ${styles.gradientbg}`}>
-        <ThreeSixty
-          currentImage={currentImage}
-          isDialogOpen={false}
-          onChangeImage={handleImageChange}
-        />
+        <ThreeSixty currentImage={currentImage} isDialogOpen={false} onChangeImage={handleImageChange} />
       </main>
       <SearchDialog />
       <Footer onImageChange={handleImageChange} showChangeScenery={true} />
     </>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
-import styles from '@/styles/Home.module.css';
-import { SearchDialog } from '@/components/SearchDialog';
-import Footer from '@/components/ui/footer';
-import ThreeSixty from '@/components/ThreeSixty';
-import CircleNav from '@/components/ui/CircleNav';
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '@/styles/Home.module.css'
+import Footer from '@/components/ui/footer'
+import CircleNav from '@/components/ui/CircleNav'
 
-const Chat = () => {
-  const [currentImage, setCurrentImage] = useState<string>('./background/bg1.jpg'); // Initial placeholder image
-  const [backgroundImages, setBackgroundImages] = useState<string[]>([]);
-
-  useEffect(() => {
-    const fetchImagesWhenIdle = () => {
-      fetch('/api/backgroundImages')
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          return response.json();
-        })
-        .then((images) => {
-          setBackgroundImages(images);
-          images.forEach((image: any) => {
-            new Image().src = `./background/${image}`;
-          });
-          const randomImage = images[Math.floor(Math.random() * images.length)];
-          setCurrentImage(`./background/${randomImage}`);
-        })
-        .catch((error) => {
-          console.error('There was a problem fetching the background images:', error);
-        });
-    };
-
-    if ('requestIdleCallback' in window) {
-      (window as any).requestIdleCallback(fetchImagesWhenIdle);
-    } else {
-      setTimeout(fetchImagesWhenIdle, 1000);
-    }
-  }, []);
-
-  const handleImageChange = () => {
-    const randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
-    setCurrentImage(`./background/${randomImage}`);
-  };
-
+export default function Home() {
   return (
     <>
       <Head>
-        <title>Chat with Alex documents while enjoying fantastic generated worlds!</title>
+        <title>Alex Welcing webs in New York.</title>
         <meta
           name="description"
-          content="Explore Alex Welcing's world using 360 and chat with documents! Learn about his experience in product leadership and how he uses generative AI and LLM to build amazing products."
+          content="Check out Alex Welcing's OpenAI experiments, Generative AI creations, and a career in technology, consulting, and marketing."
         />
-        <meta name="keywords" content="Alex Welcing, product leadership, generative AI, LLM, 360, chat with documents" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <CircleNav />
-      <main className={`${styles.main} ${styles.gradientbg}`}>
-        <ThreeSixty currentImage={currentImage} isDialogOpen={false} onChangeImage={handleImageChange} />
-      </main>
-      <SearchDialog />
-      <Footer onImageChange={handleImageChange} showChangeScenery={true} />
+      <div
+        className={styles.gradientbg + ' flex flex-col items-center justify-center min-h-screen'}
+      >
+        <div className={styles.centeredtitle}>Web experiments with Alex Welcing.</div>
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+          <Link href="/chat" className={styles.landingBtn}>
+            Chat
+          </Link>
+          <Link href="/viewer" className={styles.landingBtn}>
+            Explore
+          </Link>
+          <Link href="/about" className={styles.landingBtn}>
+            About
+          </Link>
+        </div>
+      </div>
+      <Footer
+        onImageChange={function (newImage: string): void {
+          throw new Error('Function not implemented.')
+        }}
+        showChangeScenery={false}
+      />
     </>
-  );
-};
-
-export default Chat;
+  )
+}
