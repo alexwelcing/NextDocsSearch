c0e142d4d99d1918f5c9a4bd4d9ab5782fff9147
alexwelcing <alexwelcing@gmail.com>
2023-08-15 10:28:17 -0400

    set script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,28 +2,22 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
 
 
-const GTM_ID = process.env.GTM_ID as string
-
 export default function Document() {
   return (
     <Html lang="en">
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-HZNLDCC1GR" />
-      <Head />
-      <Script id="google-tag-manager" strategy="afterInteractive">
-      {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <Head>
+        <Script id="gtm" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-      `}
-    </Script>
+        })(window,document,'script','dataLayer','GTM-W24L468');`}}></Script>
+        </Head>
       <body>
   <Main />
   <NextScript />
   <noscript
     dangerouslySetInnerHTML={{
-      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXX" height="0" width="0" style="display: none; visibility: hidden;" />`,
+      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W24L468" height="0" width="0" style="display: none; visibility: hidden;" />`,
     }}
   />
 </body>
